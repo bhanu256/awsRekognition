761b3e0b2f9f14ae8a27672c5eae4e7bdc3b8ccc
Bhanu Chowdary <bhanuphani100@gmail.com>
2020-07-14 16:50:54 +0530

    initial commit

---
+++ recognize.php
@@ -0,0 +1,82 @@
+<?php
+
+require 'aws.phar';
+
+// required classes
+use Aws\S3\S3Client;
+use Aws\Rekognition\RekognitionClient;
+
+// aws credentials
+$bucket = "localtestdev";
+$region = "ap-southeast-1";
+$access_key_id = "AKIAXO4Z5UT4L5JY4AN5";
+$secret_access_key = "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";
+
+// s3 client
+$s3 = new S3Client([
+    'region' => $region,
+    'version' => 'latest',
+    'credentials' => [
+        'key' => $access_key_id,
+        'secret' => $secret_access_key,
+    ]
+]);
+
+// aws rekognition
+$rekognition = new RekognitionClient([
+    'region' => $region,
+    'version' => 'latest',
+    'credentials' => [
+        'key' => $access_key_id,
+        'secret' => $secret_access_key,
+    ]
+]);
+
+if(isset($_REQUEST['imgUrl'])){
+    $imgUrl = $_REQUEST['imgUrl'];
+
+    try{
+        // downloads file to server
+        $tempFilePath = explode("?", basename($imgUrl))[0];
+        $tempFile = fopen($tempFilePath, "w") or die("Error: Unable to open file.");
+        $fileContents = file_get_contents($imgUrl);
+        $tempFile = file_put_contents($tempFilePath, $fileContents);
+
+        // uploads file to s3
+        $upload = $s3->putObject([
+            'Bucket' => $bucket,
+            'Key' => $tempFilePath,
+            'SourceFile' => $tempFilePath,
+            'ACL' => 'public-read',
+        ]);
+
+        // extract text from image
+        $result = $rekognition->detectText([
+            'Image' => [
+                'S3Object' => [
+                    'Bucket' => $bucket,
+                    'Name' => $tempFilePath,
+                ]
+            ]
+        ]);
+
+        // deleted temp s3 file
+        $s3del =$s3->deleteObject([
+            'Bucket' => $bucket,
+            'Key' => $tempFilePath
+        ]);
+
+        // garbage collection
+        gc_collect_cycles();
+        try {unlink($tempFilePath);} catch(Exception $ex) {}
+
+        header('Content-Type: application/json');
+        print(json_encode(array('result' => $result['TextDetections'])));
+        exit;
+    }
+    catch(Exception $exp){
+        exit($exp);
+    }
+}
+
+?>
